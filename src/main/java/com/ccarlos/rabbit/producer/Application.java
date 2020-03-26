6c45ad8e8a553e7a831e7b0496fcd132ac140362
ccarlos <2238513100@qq.com>
2020-03-26 21:28:51 +0800

    (3 4 2-14) rabbitmq与springboot整合_生产端-1

---
+++ src/main/java/com/ccarlos/rabbit/producer/Application.java
@@ -0,0 +1,17 @@
+package com.ccarlos.rabbit.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: SpringBoot主类
+ * @author: ccarlos
+ * @date: 2020/3/26 21:07
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
